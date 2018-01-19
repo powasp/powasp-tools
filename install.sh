60c1ca8a5e13a4cdb5b8de9c4bca7db14edda851
POWASP <purdueowasp@gmail.com>
2018-01-19 00:24:09 -0500

    Adding comments for clarity

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# Installs powasp-tools the first time
+
+# Ensure bin files have execute permissions
 chmod -R +x /powasp/bin
+
+# Add export path command to bashrc (so commands are available in non-login scripts too!)
 echo 'export PATH="$PATH:/powasp/bin"' >> ~/.bashrc
