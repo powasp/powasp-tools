8ae6e0c144926f5b8d038965410a2835e0aa749f
POWASP <purdueowasp@gmail.com>
2018-01-19 18:17:38 -0500

    Added checks to install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,38 @@
 
 # Installs powasp-tools the first time
 
+
+# Variables
+RCPATH="~/.bashrc"
+PROFILEPATH="~/.bash_profile"
+RCSTR="[[ -f /powasp/.bashrc ]] && source /powasp/.bashrc"
+PROFILESTR="[[ -f $PROFILEPATH ]] && source $PROFILEPATH"
+
+function checkfile () {
+  # Checks a file for a string
+  # Usage: checkfile <file> <string>
+
+  if [[ $(grep "$2" "$1)" ]]; then
+    # String found
+    return 0;
+  else
+    # String not found
+    return 1;
+}
+
+
+
 # Ensure bin files have execute permissions
 # chmod -R +x /powasp/bin
 
 # Add contents of powasp .bashrc to local .bashrc (so commands are available in non-login scripts too!)
-echo "source /powasp/.bashrc" >> ~/.bashrc
+# Only if the line isn't already there!
+if [[ $(grep $RCSTR $RCPATH) ]]; then
+  echo "$RCSTR" >> "$RCPATH"
+fi
+
+# Make sure .bash_profile pulls from .bashrc for cross-compatibility
+# Only if it hasn't been sourced already!
+if [[ $(grep "~/.bashrc" $PROFILEPATH) ]]; then
+  echo "$PROFILESTR" >> "$PROFILEPATH"
+fi
