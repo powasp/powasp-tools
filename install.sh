b1217ed02a38bc2f33285ea83a77762f4ef1f20e
POWASP <purdueowasp@gmail.com>
2018-01-20 20:45:06 -0500

    Made install.sh more verbose

--- install.sh
+++ install.sh
@@ -13,14 +13,33 @@ PROFILESTR="[[ -f ~/.bashrc ]] && source ~/.bashrc # Loads .bashrc"
 # Ensure bin files have execute permissions
 # chmod -R +x /powasp/bin
 
+
+echo "-----------"
+echo "Beginning installation..."
+echo
+
 # Add contents of powasp .bashrc to local .bashrc (so commands are available in non-login scripts too!)
 # Only if the line isn't already there!
+echo "Adding powasp/bin to ~/.bashrc..."
 if [[ -z $(grep "$RCSTR" "$RCPATH") ]]; then
-  echo "$RCSTR" >> "$RCPATH"
+  # Error handling
+  echo "$RCSTR" >> "$RCPATH" && echo "Added!" || echo "ERROR: Failed to add line to .bashrc! Try adding manually"
+else
+  echo "Already added! Skipping..."
 fi
 
+
+echo "Adding ~/.bashrc to ~/.bash_profile..."
 # Make sure .bash_profile pulls from .bashrc for cross-compatibility
 # Only if it hasn't been sourced already!
 if [[ -z $(grep "~/.bashrc" "$PROFILEPATH") ]]; then
-  echo "$PROFILESTR" >> "$PROFILEPATH"
+  # Error handling
+  echo "$PROFILESTR" >> "$PROFILEPATH" && echo "Added!" || echo "ERROR: Failed to add line to .bash_profile! Try adding manually"
+else
+  echo "Already added! Skipping..."
 fi
+
+echo
+echo "-----------"
+echo "Installation Complete!"
+echo "-----------"
